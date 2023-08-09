cc26a460a09ba218c7ef5df16798b6399cdaca41
nvkhacker <naveenkumawat09@gmail.com>
2023-08-09 18:38:42 +0530

    composer.json files added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'added/pkg-name',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '4781af001fb80916873403033f83e2f5c362c2e4',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'added/pkg-name' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '4781af001fb80916873403033f83e2f5c362c2e4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
